14a5694c153aac63c09fdb89ff02b887d7fba62c
Brady Wied <brady@wied.us>
2014-06-10 23:00:56 -0500

    Rename and populate metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 # Encoding: utf-8
-name             'gpg'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'none'
+name             'bsw_gpg'
+maintainer       'BSW Technology Consulting LLC'
+maintainer_email 'support@bswtechconsulting.com'
 license          ''
-description      'Installs/Configures gpg'
+description      'Installs/Configures gpg using an LWRP based approach'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
