efc119c2644381c48b3e1b87b13d692ce43adfa3
Brady Wied <brady@wied.us>
2014-06-10 21:49:04 -0500

    Different user test

--- test/unit/spec/lwrp_key_manage_spec.rb
+++ test/unit/spec/lwrp_key_manage_spec.rb
@@ -144,18 +144,51 @@ describe 'gpg::lwrp:key_manage' do
   end
 
   it 'works properly when run as a different user' do
-    # arrange
+    executed = []
+    @stub_setup = lambda do |shell_out|
+      executed << shell_out
+      case shell_out.command
+        when 'gpg2 --import --no-default-keyring --secret-keyring temp_file_0 --keyring temp_file_1'
+          shell_out.stub!(:error!)
+        when 'gpg2 --list-keys --fingerprint --no-default-keyring --keyring temp_file_1'
+          shell_out.stub!(:error!)
+          shell_out.stub!(:stdout).and_return <<-EOF
+        -----------------
+        pub   2048R/390AA6C9 2014-06-10 [expires: 2016-06-09]
+              Key fingerprint = 4D1C F328 8469 F260 C211  9B9F 76C9 5D74 390A A6C9
+        uid                  BSW Tech DB Backup db_dev (WAL-E/S3 Encryption key) <db_dev@wale.backup.bswtechconsulting.com>
+        sub   2048R/1A0B6924 2014-06-10 [expires: 2016-06-09]
+          EOF
+        when 'gpg2 --list-keys --fingerprint'
+          shell_out.stub!(:error!)
+          shell_out.stub!(:stdout).and_return ''
+        when 'gpg2 --import'
+          shell_out.stub!(:error!)
+        when 'shred -n 20 -z temp_file_0'
+          shell_out.stub!(:error!)
+        else
+          shell_out.stub(:error!).and_raise "Unexpected command #{shell_out.command}"
+      end
+    end
+
+    # act
     temp_lwrp_recipe contents: <<-EOF
-      gpg_key_manage 'someoneelse' do
+      gpg_key_manage 'someone_else' do
         key_contents 'thekeybitshere'
       end
     EOF
 
-    # act
-
     # assert
-    pending 'test the updated set to true on new_resource, use standard approach in bsw_pgsql cookbook'
-    pending 'Write this test'
+    expect(executed).to have(5).items
+    executed[0].user.should == 'someone_else'
+    executed[0].input.should == 'thekeybitshere'
+    executed[1].user.should == 'someone_else'
+    executed[2].user.should == 'someone_else'
+    executed[3].user.should == 'someone_else'
+    executed[3].input.should == 'thekeybitshere'
+    executed[4].user.should == 'someone_else'
+    resource = @chef_run.find_resource 'gpg_key_manage', 'someone_else'
+    expect(resource.updated_by_last_action?).to eq(true)
   end
 
   it 'overwrites the existing key for the user if the fingerprint has changed' do
