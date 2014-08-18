1d2b682fdeb3c0918b89f5900ea5014ccf8eedd9
Brady Wied <brady@wied.us>
2014-08-18 00:50:10 -0500

    Better defaults

--- resources/key_manage.rb
+++ resources/key_manage.rb
@@ -4,4 +4,4 @@ default_action :replace
 attribute :for_user, :kind_of => String, :name_attribute => true
 attribute :key_contents, :kind_of => String
 attribute :chef_vault_info, :kind_of => Hash
-attribute :key_type, :kind_of => Symbol, :default => :public, :equal_to => [:public, :private]
+attribute :key_type, :kind_of => Symbol, :default => :secret_key, :equal_to => [:public_key, :secret_key]
