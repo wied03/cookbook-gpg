55c13f59e8b4d303be1a3ad6cd6b935863f20f58
Brady Wied <brady@wied.us>
2014-08-20 11:20:21 -0500

    Add shell resource + provider

---
+++ libraries/provider_load_key_from_key_server.rb
@@ -0,0 +1,20 @@
+class Chef
+  class Provider
+    class BswGpgLoadKeyFromKeyServer < BaseGpgProvider
+      def initialize(new_resource, run_context)
+        super
+      end
+
+      def load_current_resource
+        @current_resource ||= Chef::Resource::BswGpgLoadKeyFromKeyServer.new(new_resource.name)
+        @current_resource.key_server(new_resource.key_server)
+        @current_resource.key_id(new_resource.key_id)
+        super
+      end
+
+      def get_key
+        raise 'not implemented yet'
+      end
+    end
+  end
+end
