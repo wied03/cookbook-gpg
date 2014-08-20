f17242ea57f2638524b5a1d9a2f8f82bd040875a
Brady Wied <brady@wied.us>
2014-08-19 23:32:13 -0500

    Explain why

--- libraries/load_key_base_resource.rb
+++ libraries/load_key_base_resource.rb
@@ -5,6 +5,7 @@ class Chef
       actions :replace
 
       def initialize(name, run_context=nil)
+        # Only reason these are here and not set in the class is that we inherit this
         self.class.resource_name = self.class.snake_case_basename self.class.name
         self.class.default_action :replace
         super
