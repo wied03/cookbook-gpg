4c9d20bce77b8848bc1dd5a5befd2fc26a3f00d0
Brady Wied <brady@wied.us>
2014-08-20 12:47:31 -0500

    Simplify with shared mock code

--- test/unit/spec/lwrp_load_key_from_chef_vault_spec.rb
+++ test/unit/spec/lwrp_load_key_from_chef_vault_spec.rb
@@ -9,14 +9,6 @@ require 'helper_key_header'
 describe 'gpg::lwrp:load_key_from_chef_vault' do
   include BswTech::ChefSpec::LwrpTestHelper
 
-  before {
-    stub_resources
-  }
-
-  after(:each) {
-    cleanup
-  }
-
   def cookbook_under_test
     'bsw_gpg'
   end
@@ -25,56 +17,11 @@ describe 'gpg::lwrp:load_key_from_chef_vault' do
     'load_key_from_chef_vault'
   end
 
-  before {
-    @stub_setup = nil
-    original_new = Mixlib::ShellOut.method(:new)
-    Mixlib::ShellOut.stub!(:new) do |*args|
-      cmd = original_new.call(*args)
-      cmd.stub!(:run_command)
-      @stub_setup.call(cmd) if @stub_setup
-      cmd
-    end
-    @gpg_retriever = double()
-    BswTech::Gpg::GpgRetriever.stub(:new).and_return(@gpg_retriever)
-    @current_type_checked = nil
-    @external_type = nil
-    @base64_used = nil
-    @shell_outs = []
-  }
-
-  def stub_retriever(current=[], draft)
-    allow(@gpg_retriever).to receive(:get_current_installed_keys) do |executor, type|
-      @current_type_checked = type
-      current
-    end
-    allow(@gpg_retriever).to receive(:get_key_info_from_base64) do |executor, type, base64|
-      @external_type = type
-      @base64_used = base64
-      draft
-    end
-  end
-
-  def executed_command_lines
-    @shell_outs.inject({}) do |total, item|
-      total[item.command] = item.input
-      total
-    end
-  end
-
   ['data_bag', 'item', 'json_key', 'for_user'].each do |attr_to_include|
     it "fails if we only supply #{attr_to_include}" do
       # arrange
       # Include all of this because for_user will try and run the provider's constructor
-      @stub_setup = lambda do |shell_out|
-        case shell_out.command
-          when '/bin/sh -c "echo -n ~value"'
-            shell_out.stub!(:error!)
-            shell_out.stub!(:stdout).and_return('/home/root')
-          else
-            shell_out.stub(:error!).and_raise "Unexpected command #{shell_out.command}"
-        end
-      end
-
+      setup_stub_commands([:command => '/bin/sh -c "echo -n ~value"', :stdout => '/home/root'])
       # act
       action = lambda {
         temp_lwrp_recipe <<-EOF
@@ -92,23 +39,23 @@ describe 'gpg::lwrp:load_key_from_chef_vault' do
   it 'allows supplying Chef vault info for a private key directly as opposed to key contents' do
     # arrange
     stub_retriever(draft=BswTech::Gpg::KeyHeader.new(fingerprint='4D1CF3288469F260C2119B9F76C95D74390AA6C9',
-                                                      username='the username',
-                                                      id='the id',
-                                                      type=:secret_key))
-    @stub_setup = lambda do |shell_out|
-      @shell_outs << shell_out
-      case shell_out.command
-        when '/bin/sh -c "echo -n ~root"'
-          shell_out.stub!(:error!)
-          shell_out.stub!(:stdout).and_return('/home/root')
-        when 'gpg2 --import'
-          shell_out.stub!(:error!)
-        when 'gpg2 --import-ownertrust'
-          shell_out.stub!(:error!)
-        else
-          shell_out.stub(:error!).and_raise "Unexpected command #{shell_out.command}"
-      end
-    end
+                                                     username='the username',
+                                                     id='the id',
+                                                     type=:secret_key))
+    setup_stub_commands([
+                            {
+                                :command => '/bin/sh -c "echo -n ~root"',
+                                :stdout => '/home/root'
+                            },
+                            {
+                                :command => 'gpg2 --import',
+                                :expected_input => '-----BEGIN PGP PRIVATE KEY BLOCK-----'
+                            },
+                            {
+                                :command => 'gpg2 --import-ownertrust',
+                                :expected_input => "4D1CF3288469F260C2119B9F76C95D74390AA6C9:6:\n"
+                            }
+                        ])
     stub_vault_entry = {'json_key' => '-----BEGIN PGP PRIVATE KEY BLOCK-----'}
     ChefVault::Item.stub!(:load).with('thedatabag', 'the_item').and_return stub_vault_entry
 
@@ -130,14 +77,7 @@ describe 'gpg::lwrp:load_key_from_chef_vault' do
     executed_cmdline.keys.should == ['/bin/sh -c "echo -n ~root"',
                                      'gpg2 --import',
                                      'gpg2 --import-ownertrust']
-    users = @shell_outs.map { |e| e.user }.uniq
-    users.should == ['root']
-    env = @shell_outs.map { |e| e.environment['HOME'] }.uniq
-    # 1st call is to get home dir, so won't be there yet
-    env.should == [nil, '/home/root']
-    input_specified = executed_cmdline.reject { |k, v| !v }
-    input_specified.should == {'gpg2 --import' => '-----BEGIN PGP PRIVATE KEY BLOCK-----',
-                               'gpg2 --import-ownertrust' => "4D1CF3288469F260C2119B9F76C95D74390AA6C9:6:\n"}
+    verify_actual_commands_match_expected
     resource = @chef_run.find_resource 'bsw_gpg_load_key_from_chef_vault', 'some key'
     expect(resource.updated_by_last_action?).to eq(true)
   end
