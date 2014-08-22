c5ba60dd27e9c97137ac7268e7ea120821d8a977
Brady Wied <brady@wied.us>
2014-08-21 20:10:14 -0500

    Include the supported platforms

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 # Encoding: utf-8
-name             'bsw_gpg'
-maintainer       'BSW Technology Consulting LLC'
+name 'bsw_gpg'
+maintainer 'BSW Technology Consulting LLC'
 maintainer_email 'support@bswtechconsulting.com'
-license          'BSD 2 Clause License'
-description      'Installs/Configures gpg using an LWRP based approach'
+license 'BSD 2 Clause License'
+description 'Installs/Configures gpg using an LWRP based approach'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version '0.1.3'
+supports 'ubuntu', '14.04'
