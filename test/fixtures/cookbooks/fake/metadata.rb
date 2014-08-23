9c661d98f63b8e70034807b3f57c0d5a7338337f
Brady Wied <brady@wied.us>
2014-08-23 14:42:50 -0500

    * Remove chef vault cookbook that was not really helping us * Remove users we don't need * Add test for walt

--- test/fixtures/cookbooks/fake/metadata.rb
+++ test/fixtures/cookbooks/fake/metadata.rb
@@ -2,4 +2,3 @@ name 'fake'
 version '1.0.0'
 
 depends 'bsw_gpg'
-depends 'chef-vault', '1.1.2'
--- test/fixtures/cookbooks/fake/recipes/from_chef_vault.rb
+++ test/fixtures/cookbooks/fake/recipes/from_chef_vault.rb
@@ -1,5 +1,6 @@
 private_key_bits = cookbook_file_contents 'joe_secret.pem', 'fake'
 
+# Need this to write to data bags, etc.
 chef_gem 'cheffish' do
   version '0.7.1'
 end
@@ -9,38 +10,13 @@ chef_gem 'chef-vault' do
 end
 
 require 'cheffish'
-require 'chef-vault' # for my testing
+require 'chef-vault'
 
 with 'walt', 'a-secret-one', 'an-item' do |username, data_bag_name, vault_item|
-  # This is returning results
-  #stuff = search(:node,'*:*')
-  #puts "stuff is #{stuff}"
-  # query = Chef::Search::Query.new
-  # result = query.search(:node, '*:*')
-  # puts "manual result is #{result}"
-  #
-  #
   item = ChefVault::Item.new(data_bag_name, vault_item)
   item.clients('*:*')
-  #item.admins('')
-  # this is not
   item['json_key'] = private_key_bits
   item.save
-
-  # chef_vault_secret vault_item do
-  #   data_bag data_bag_name
-  #   raw_data({'json_key' => private_key_bits})
-  #   #admins 'dummy_admin'
-  #   admins ''
-  #   search '*:*'
-  # end
-
-
-  ruby_block 'stuff' do
-    block do
-      puts "output of data bags dir: "+`cat /tmp/kitchen/data_bags/a-secret-one/an-item.json`
-    end
-  end
 
   user_with_home(self, username)
 
