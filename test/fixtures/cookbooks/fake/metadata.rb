b361d28d467d3f40cb45269f2a804bf8e60999fc
Brady Wied <brady@wied.us>
2014-11-24 16:01:09 -0600

    Test with SELinux enabled, passes

--- test/fixtures/cookbooks/fake/metadata.rb
+++ test/fixtures/cookbooks/fake/metadata.rb
@@ -3,3 +3,4 @@ version '1.0.0'
 
 depends 'bsw_gpg'
 depends 'build-essential'
+depends 'selinux', '0.8.0'
--- test/fixtures/cookbooks/fake/recipes/default.rb
+++ test/fixtures/cookbooks/fake/recipes/default.rb
@@ -3,6 +3,10 @@ include_recipe 'bsw_gpg::default'
 chef_gem 'with'
 require 'with'
 
+selinux_state 'SELinux Compatible' do
+  action :enforcing
+end
+
 include_recipe 'fake::from_string'
 include_recipe 'fake::from_key_server'
 include_recipe 'fake::from_chef_vault'
